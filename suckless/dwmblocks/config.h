834a0cd9e44744e0d27a9f69b6bc6d42d2039ea9
dhrxvxd-arch <dhrxvxd@gmail.com>
2026-01-08 16:30:34 +0530

    dwmblocks

---
+++ suckless/dwmblocks/config.h
@@ -0,0 +1,11 @@
+static const Block blocks[] = {
+
+/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"",          "volume",               1,                      10},
+  {"",  	"clock",  		60,			1},
+  {"",  	"internet", 		5,			4},
+  {"",          "battery",              120,                    1},
+};
+
+static char delim[] = "  |  ";
+static unsigned int delimLen = 5;
